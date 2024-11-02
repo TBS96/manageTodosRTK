e731a268858f8a345257e401ea2dc982dd0936dc
TBS96 <9tbs6@proton.me>
2024-11-02 20:52:40 +0530

    added alert if user clicks addTodoHandler btn without entering any task
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -10,6 +10,7 @@ const AddTodo = () => {
 
     const addTodoHandler = (e) => {
         e.preventDefault()
+        if (!input) return alert('Please enter a task!')
         dispatch(addTodo(input))
         setInput('')
     }
